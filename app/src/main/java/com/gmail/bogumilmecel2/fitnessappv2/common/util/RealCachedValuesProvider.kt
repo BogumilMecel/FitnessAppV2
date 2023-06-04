0f5a3f439772b2664a47d2650d1105e05ba26067
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-06-04 18:23:54 +0200

    small work

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/RealCachedValuesProvider.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/RealCachedValuesProvider.kt
@@ -1,8 +1,10 @@
 package com.gmail.bogumilmecel2.fitnessappv2.common.util
 
 import android.content.SharedPreferences
+import com.gmail.bogumilmecel2.fitnessappv2.common.data.utils.CustomSharedPreferencesUtils
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.CachedValuesProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
+import com.gmail.bogumilmecel2.fitnessappv2.feature_auth.domain.model.User
 import com.gmail.bogumilmecel2.fitnessappv2.feature_summary.domain.model.LogEntry
 import com.gmail.bogumilmecel2.fitnessappv2.feature_weight.domain.model.WeightEntry
 import com.google.gson.Gson
@@ -11,33 +13,40 @@ class RealCachedValuesProvider(
     private val gson: Gson,
     private val sharedPreferences: SharedPreferences
 ): CachedValuesProvider {
-    override fun getWantedNutritionValues(): NutritionValues {
+
+    companion object {
+        const val USER_KEY = "user"
+    }
+
+    override suspend fun getWantedNutritionValues(): NutritionValues {
         TODO("Not yet implemented")
     }
 
-    override fun saveWantedNutritionValues(nutritionValues: NutritionValues) {
+    override suspend fun saveWantedNutritionValues(nutritionValues: NutritionValues) {
         TODO("Not yet implemented")
     }
 
-    override fun getWeightProgress(): String {
+    override suspend fun getWeightProgress(): String {
         TODO("Not yet implemented")
     }
 
-    override fun updateWeightIProgress() {
+    override suspend fun updateWeightIProgress() {
         TODO("Not yet implemented")
     }
 
-    override fun getLatestLogEntry(): LogEntry {
+    override suspend fun getLatestLogEntry(): LogEntry {
         TODO("Not yet implemented")
     }
 
-    override fun getLatestWeightEntry(): WeightEntry {
+    override suspend fun getLatestWeightEntry(): WeightEntry {
         TODO("Not yet implemented")
     }
 
-    override fun updateLatestWeightEntry() {
+    override suspend fun updateLatestWeightEntry() {
         TODO("Not yet implemented")
     }
+
+    private suspend fun getUser() = getItemFromJson(CustomSharedPreferencesUtils.USER_KEY, User::class.java)
 
     private fun <T> getItemFromJson(key: String, clazz: Class<T>): T?{
         return try {
