ad0fa660d5142a3e54b4b7dbaef11ab0e021376e
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-07-12 13:35:36 +0200

    test fixes

--- app/src/test/java/com/gmail/bogumilmecel2/fitnessappv2/feature_introduction/domain/use_cases/SaveIntroductionInformationUseCaseTest.kt
+++ app/src/test/java/com/gmail/bogumilmecel2/fitnessappv2/feature_introduction/domain/use_cases/SaveIntroductionInformationUseCaseTest.kt
@@ -1,132 +1,207 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.use_cases
 
-import com.gmail.bogumilmecel2.fitnessappv2.common.BaseTest
+import com.gmail.bogumilmecel2.fitnessappv2.R
+import com.gmail.bogumilmecel2.fitnessappv2.common.BaseMockkTest
+import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
+import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.UserInformation
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.Resource
-import com.gmail.bogumilmecel2.fitnessappv2.feature_auth.domain.model.User
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.ActivityLevel
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.DesiredWeight
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.Gender
+import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.IntroductionResponse
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.TrainingFrequency
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.model.TypeOfWork
 import com.gmail.bogumilmecel2.fitnessappv2.feature_introduction.domain.repository.UserDataRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.kotlin.any
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
-@OptIn(ExperimentalCoroutinesApi::class)
-internal class SaveIntroductionInformationUseCaseTest : BaseTest() {
+internal class SaveIntroductionInformationUseCaseTest : BaseMockkTest() {
+    
+    companion object {
+        const val AGE_ERROR = "age error"
+        const val FIELD_ERROR = "field error"
+        const val HEIGHT_ERROR = "height error"
+        const val WEIGHT_ERROR = "weight error"
+    }
 
-    private lateinit var saveIntroductionInformationUseCase: SaveIntroductionInformationUseCase
-
-    @Mock
-    private lateinit var mockUserDataRepository: UserDataRepository
-
+    private val userDataRepository = mockk<UserDataRepository>()
+    private val saveIntroductionInformationUseCase = SaveIntroductionInformationUseCase(
+        userDataRepository = userDataRepository,
+        resourceProvider = resourceProvider
+    )
+    
     @Before
-    fun setUp() = runTest {
-        mockUserDataRepository = Mockito.mock(UserDataRepository::class.java)
-        mockRepositoryResponse()
-
-        saveIntroductionInformationUseCase = SaveIntroductionInformationUseCase(
-            resourceProvider = resourceProvider,
-            userDataRepository = mockUserDataRepository
+    fun setUp() {
+        mockString(
+            resId = R.string.please_make_sure_all_fields_are_filled_in_correctly,
+            value = FIELD_ERROR
+        )
+        mockString(
+            resId = R.string.please_make_sure_you_have_entered_your_age,
+            value = AGE_ERROR
+        )
+        mockString(
+            resId = R.string.introduction_height_error,
+            value = HEIGHT_ERROR
+        )
+        mockString(
+            resId = R.string.please_make_sure_you_have_entered_your_weight,
+            value = WEIGHT_ERROR
         )
     }
 
     @Test
-    fun `if called with empty weight, return resource error`() = runTest {
-        val result = callWithCorrectValues(weight = "")
-        assertTrue(result is Resource.Error)
+    fun `if called with empty weight, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(weight = "")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with weight less than 0, return resource error`() = runTest {
-        val result = callWithCorrectValues(weight = "-1")
-        assertTrue(result is Resource.Error)
+    fun `if called with weight less than 0, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(weight = "-1")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = WEIGHT_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with weight higher than 500, return resource error`() = runTest {
-        val result = callWithCorrectValues(weight = "501")
-        assertTrue(result is Resource.Error)
+    fun `if called with weight higher than 500, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(weight = "501")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = WEIGHT_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with incorrect weight, return resource error`() = runTest {
-        val result = callWithCorrectValues(weight = "abc")
-        assertTrue(result is Resource.Error)
+    fun `if called with incorrect weight, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(weight = "abc")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with empty height, return resource error`() = runTest {
-        val result = callWithCorrectValues(height = "")
-        assertTrue(result is Resource.Error)
+    fun `if called with empty height, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(height = "")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with height less than 0, return resource error`() = runTest {
-        val result = callWithCorrectValues(height = "-1")
-        assertTrue(result is Resource.Error)
+    fun `if called with height less than 0, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(height = "-1")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = HEIGHT_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with height higher than 250, return resource error`() = runTest {
-        val result = callWithCorrectValues(height = "251")
-        assertTrue(result is Resource.Error)
+    fun `if called with height higher than 250, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(height = "251")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = HEIGHT_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with incorrect height, return resource error`() = runTest {
-        val result = callWithCorrectValues(height = "abc")
-        assertTrue(result is Resource.Error)
+    fun `if called with incorrect height, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(height = "abc")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with empty age, return resource error`() = runTest {
-        val result = callWithCorrectValues(age = "")
-        assertTrue(result is Resource.Error)
+    fun `if called with empty age, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(age = "")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with age less than 0, return resource error`() = runTest {
-        val result = callWithCorrectValues(age = "-1")
-        assertTrue(result is Resource.Error)
+    fun `if called with age less than 0, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(age = "-1")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = AGE_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with age higher than 100, return resource error`() = runTest {
-        val result = callWithCorrectValues(age = "101")
-        assertTrue(result is Resource.Error)
+    fun `if called with age higher than 100, return resource error with correct text`() = runTest {
+        val resource = callWithCorrectValues(age = "101")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = AGE_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
     fun `if called with incorrect age, return resource error`() = runTest {
-        val result = callWithCorrectValues(age = "abc")
-        assertTrue(result is Resource.Error)
+        val resource = callWithCorrectValues(age = "abc")
+        assertIs<Resource.Error<Unit>>(resource)
+        assertEquals(
+            expected = FIELD_ERROR,
+            resource.uiText
+        )
     }
 
     @Test
-    fun `if called with proper data and repository return resource success, return resource success`() = runTest {
-        val result = callWithCorrectValues()
-        assertTrue(result is Resource.Success)
-    }
+    fun `if called with proper data and repository return resource success, return resource success`() =
+        runTest {
+            val nutritionValues = NutritionValues()
+            val userInformation = UserInformation()
+            coEvery { userDataRepository.saveUserInformation(any()) } returns Resource.Success(
+                data = IntroductionResponse(
+                    nutritionValues = nutritionValues,
+                    userInformation = userInformation
+                )
+            )
+            coEvery { cachedValuesProvider.saveWantedNutritionValues(nutritionValues) } returns Unit
+            coEvery { cachedValuesProvider.updateUserInformation(userInformation) } returns Unit
+            assertIs<Resource.Success<Unit>>(callWithCorrectValues())
+            coVerify(exactly = 1){
+                cachedValuesProvider.saveWantedNutritionValues(nutritionValues)
+                cachedValuesProvider.updateUserInformation(userInformation)
+            }
+        }
 
     @Test
-    fun `if called with proper data and repository return resource error, return resource error`() = runTest {
-        mockRepositoryResponse(response = Resource.Error())
-        val result = callWithCorrectValues()
-        assertTrue(result is Resource.Error)
-    }
-
-    private suspend fun mockRepositoryResponse(
-        response: Resource<User> = Resource.Success(data = User())
-    ) {
-        Mockito.`when`(mockUserDataRepository.saveUserInformation(any())).thenReturn(response)
-    }
+    fun `if called with proper data and repository return resource error, return resource error`() =
+        runTest {
+            coEvery { userDataRepository.saveUserInformation(any()) } returns Resource.Error()
+            assertIs<Resource.Error<Unit>>(callWithCorrectValues())
+        }
 
     private suspend fun callWithCorrectValues(
         selectedGender: Gender = Gender.MALE,
@@ -137,14 +212,15 @@ internal class SaveIntroductionInformationUseCaseTest : BaseTest() {
         activityLevel: ActivityLevel = ActivityLevel.MODERATE,
         trainingFrequency: TrainingFrequency = TrainingFrequency.AVERAGE,
         desiredWeight: DesiredWeight = DesiredWeight.KEEP
-    ): Resource<User> = saveIntroductionInformationUseCase(
-            selectedGender = selectedGender,
-            age = age,
-            height = height,
-            weight = weight,
-            typeOfWork = typeOfWork,
-            activityLevel = activityLevel,
-            trainingFrequency = trainingFrequency,
-            desiredWeight = desiredWeight
-        )
+    ) = saveIntroductionInformationUseCase(
+        selectedGender = selectedGender,
+        age = age,
+        height = height,
+        weight = weight,
+        typeOfWork = typeOfWork,
+        activityLevel = activityLevel,
+        trainingFrequency = trainingFrequency,
+        desiredWeight = desiredWeight,
+        cachedValuesProvider = cachedValuesProvider
+    )
 }
--- app/src/test/java/com/gmail/bogumilmecel2/fitnessappv2/feature_summary/domain/use_case/CheckIfShouldAskForWeightDialogsUseCaseTest.kt
+++ app/src/test/java/com/gmail/bogumilmecel2/fitnessappv2/feature_summary/domain/use_case/CheckIfShouldAskForWeightDialogsUseCaseTest.kt
@@ -1,36 +1,56 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_summary.domain.use_case
 
 import com.gmail.bogumilmecel2.fitnessappv2.common.BaseMockkTest
-import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.CachedValuesProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.Resource
 import com.gmail.bogumilmecel2.fitnessappv2.feature_auth.domain.model.User
+import com.gmail.bogumilmecel2.fitnessappv2.feature_weight.domain.model.WeightDialogs
 import com.gmail.bogumilmecel2.fitnessappv2.feature_weight.domain.repository.WeightRepository
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import kotlin.test.assertIs
 
-class CheckIfShouldAskForWeightDialogsUseCaseTest: BaseMockkTest() {
+class CheckIfShouldAskForWeightDialogsUseCaseTest : BaseMockkTest() {
 
     private val weightRepository = mockk<WeightRepository>()
     private val checkIfShouldAskForWeightDialogsUseCase =
         CheckIfShouldAskForWeightDialogsUseCase(weightRepository = weightRepository)
-    private val cachedValuesProvider = mockk<CachedValuesProvider>()
     private val mockedDate = "2023-12-12"
     private val mockedDate2 = "2022-12-12"
 
     @Test
-    fun `check if get user returns null, resource error is returned`() =
+    fun `check if user has already accepted weight dialogs, resource error is returned`() =
         runTest {
-            mockData(user = null)
+            mockData(weightDialogs = mockWeightDialogs(accepted = true))
             assertIs<Resource.Error<Unit>>(callTestedMethod())
         }
 
     @Test
-    fun `check if user has already been asked for weight dialogs, resource error is returned`() =
+    fun `check if user has already seen weight dialogs offline, resource error is returned`() =
         runTest {
-            mockData(user = User(weightDialogsAccepted = true))
+            mockData(localLastTimeAskedForWeightDialogsDate = mockedDate)
+            mockDateString(mockedDate)
+            assertIs<Resource.Error<Unit>>(callTestedMethod())
+        }
+
+    @Test
+    fun `check if user has already seen weight dialogs online, resource error is returned and online date is saved offline`() =
+        runTest {
+            coEvery { cachedValuesProvider.updateLocalLastTimeAskedForWeightDialogs(mockedDate) } returns Unit
+            mockData(weightDialogs = mockWeightDialogs(lastTimeAsked = mockedDate))
+            mockDateString(mockedDate)
+            assertIs<Resource.Error<Unit>>(callTestedMethod())
+            coVerify(exactly = 1) {
+                cachedValuesProvider.updateLocalLastTimeAskedForWeightDialogs(mockedDate)
+            }
+        }
+
+    @Test
+    fun `check if user has already been asked 3 times, resource error is returned`() =
+        runTest {
+            mockData(weightDialogs = mockWeightDialogs(askedCount = 4))
             assertIs<Resource.Error<Unit>>(callTestedMethod())
         }
 
@@ -44,35 +64,31 @@ class CheckIfShouldAskForWeightDialogsUseCaseTest: BaseMockkTest() {
     @Test
     fun `check if repository returns resource success and everything else is correct, resource success is returned`() =
         runTest {
-            mockData(User(lastTimeAskedAboutWeightDialogs = mockedDate))
-            mockDateString(value = mockedDate2)
+            mockData()
+            mockDateString(value = mockedDate)
             assertIs<Resource.Success<Unit>>(callTestedMethod())
         }
 
-    @Test
-    fun `check if user has been already asked today, resource error is returned`() =
-        runTest {
-            mockData(User(lastTimeAskedAboutWeightDialogs = mockedDate))
-            mockDateString(mockedDate)
-            assertIs<Resource.Error<Unit>>(callTestedMethod())
-        }
-
-    @Test
-    fun `check if user has not been already asked today, resource error is returned`() =
-        runTest {
-            mockData(User(lastTimeAskedAboutWeightDialogs = mockedDate))
-            mockDateString(mockedDate)
-            assertIs<Resource.Error<Unit>>(callTestedMethod())
-        }
-
     private fun mockData(
-        user: User? = User(weightDialogsAccepted = false),
-        repositoryResponse: Resource<Unit> = Resource.Success(Unit)
+        weightDialogs: WeightDialogs = mockWeightDialogs(),
+        repositoryResponse: Resource<Unit> = Resource.Success(Unit),
+        localLastTimeAskedForWeightDialogsDate: String = mockedDate2
     ) {
-        coEvery { cachedValuesProvider.getUser() } returns user
+        coEvery { cachedValuesProvider.getLocalLastTimeAskedForWeightDialogs() } returns localLastTimeAskedForWeightDialogsDate
+        coEvery { cachedValuesProvider.getUser() } returns User(weightDialogs = weightDialogs)
         coEvery { weightRepository.checkIfShouldAskForWeightDialogs() } returns repositoryResponse
     }
 
     private suspend fun callTestedMethod() =
         checkIfShouldAskForWeightDialogsUseCase(cachedValuesProvider)
+
+    private fun mockWeightDialogs(
+        accepted: Boolean? = false,
+        lastTimeAsked: String = mockedDate2,
+        askedCount: Int = 3
+    ) = WeightDialogs(
+        accepted = accepted,
+        lastTimeAsked = lastTimeAsked,
+        askedCount = askedCount
+    )
 }
