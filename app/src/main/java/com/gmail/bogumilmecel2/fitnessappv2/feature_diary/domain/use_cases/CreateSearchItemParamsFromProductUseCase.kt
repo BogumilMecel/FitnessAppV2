973a246d15b70f9d9d08185852a2aad5b1078c90
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-07-31 14:11:22 +0200

    Fix search items for new recipe

---
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/CreateSearchItemParamsFromProductUseCase.kt
@@ -0,0 +1,22 @@
+package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases
+
+import com.gmail.bogumilmecel2.fitnessappv2.R
+import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.ResourceProvider
+import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.Product
+import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.presentation.search.componens.SearchItemParams
+
+class CreateSearchItemParamsFromProductUseCase(private val resourceProvider: ResourceProvider) {
+    operator fun invoke(
+        product: Product,
+        onClick: () -> Unit,
+        onLongClick: () -> Unit
+    ): SearchItemParams {
+        return SearchItemParams(
+            name = product.name,
+            textBelowName = resourceProvider.getString(R.string.measurement_unit_gram_with_value, "100"),
+            endText = resourceProvider.getString(R.string.kcal_with_value, product.nutritionValues.calories),
+            onItemClick = onClick,
+            onItemLongClick = onLongClick
+        )
+    }
+}
