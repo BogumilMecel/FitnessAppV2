c6d967094dd7140672f03c28ff2cfd5ee4a9079d
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-02 13:20:37 +0200

    Change search item look

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/presentation/search/componens/SearchList.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/presentation/search/componens/SearchList.kt
@@ -3,14 +3,18 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.presentation.search.c
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.gmail.bogumilmecel2.ui.theme.FitnessAppTheme
 
 @Composable
 fun SearchList(items: List<SearchItemParams>) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         items(items) { item ->
             SearchItem(searchItemParams = item)
+            SearchListDivider()
         }
     }
 }
@@ -19,5 +23,15 @@ fun SearchList(items: List<SearchItemParams>) {
 fun ForEachSearchList(items: List<SearchItemParams>) {
     items.forEach {
         SearchItem(searchItemParams = it)
+        SearchListDivider()
     }
+}
+
+@Composable
+private fun SearchListDivider() {
+    Divider(
+        thickness = 1.dp,
+        modifier = Modifier.fillMaxWidth(),
+        color = FitnessAppTheme.colors.BackgroundQuaternary
+    )
 }
