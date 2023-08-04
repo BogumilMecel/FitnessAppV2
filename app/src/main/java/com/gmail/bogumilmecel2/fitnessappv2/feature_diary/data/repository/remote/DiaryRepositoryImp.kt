adb92bae0c4b2ed074157bfb55f81194f67b45bd
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-04 20:52:36 +0200

    Implement getting history in search screen

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/data/repository/remote/DiaryRepositoryImp.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/data/repository/remote/DiaryRepositoryImp.kt
@@ -188,9 +188,15 @@ class DiaryRepositoryImp(
         }
     }
 
-    override suspend fun getOfflineDiaryHistory(limit: Int): Resource<List<ProductDiaryHistoryItem>> {
+    override suspend fun getLocalDiaryHistory(limit: Int): Resource<List<ProductDiaryHistoryItem>> {
         return handleRequest {
             userDiaryItemsDao.getDiaryHistory(limit = limit)
+        }
+    }
+
+    override suspend fun getLocalDiaryHistory(): Resource<List<ProductDiaryHistoryItem>> {
+        return handleRequest {
+            userDiaryItemsDao.getDiaryHistory()
         }
     }
 
--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/search/SearchDiaryUseCases.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/search/SearchDiaryUseCases.kt
@@ -1,5 +1,6 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.search
 
+import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CreateSearchItemParamsFromHistoryItemUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CreateSearchItemParamsFromProductUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CreateSearchItemParamsFromRecipeUseCase
 
@@ -8,5 +9,6 @@ data class SearchDiaryUseCases(
     val searchForProductWithBarcode: SearchForProductWithBarcode,
     val searchForRecipes: SearchForRecipes,
     val createSearchItemParamsFromRecipeUseCase: CreateSearchItemParamsFromRecipeUseCase,
-    val createSearchItemParamsFromProductUseCase: CreateSearchItemParamsFromProductUseCase
+    val createSearchItemParamsFromProductUseCase: CreateSearchItemParamsFromProductUseCase,
+    val createSearchItemParamsFromHistoryItemUseCase: CreateSearchItemParamsFromHistoryItemUseCase
 )
