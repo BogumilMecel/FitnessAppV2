4368ebd246944d03faa55dfbc81ae462a4a22851
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-14 12:16:22 +0200

    Add device id to headers

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/DefaultInterceptor.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/DefaultInterceptor.kt
@@ -1,21 +1,39 @@
 package com.gmail.bogumilmecel2.fitnessappv2.common.util
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.SharedPreferences
+import android.provider.Settings
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.Country
 import kotlinx.datetime.TimeZone
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class DefaultInterceptor(
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
+    private val applicationContext: Context
 ) : Interceptor {
+    @SuppressLint("HardwareIds")
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
         sharedPreferences.getString("token", null)?.let {
             request.addHeader("Authorization", "Bearer $it")
         }
-        request.addHeader(ApiConstants.Headers.COUNTRY, Country.POLAND.shortName)
-        request.addHeader(ApiConstants.Headers.TIMEZONE, TimeZone.currentSystemDefault().id)
+        request.addHeader(
+            name = ApiConstants.Headers.COUNTRY,
+            value = Country.POLAND.shortName
+        )
+        request.addHeader(
+            name = ApiConstants.Headers.TIMEZONE,
+            value = TimeZone.currentSystemDefault().id
+        )
+        request.addHeader(
+            name = ApiConstants.Headers.DEVICE_ID,
+            value = Settings.Secure.getString(
+                applicationContext.contentResolver,
+                Settings.Secure.ANDROID_ID
+            )
+        )
 
         val response = chain.proceed(request.build())
 
