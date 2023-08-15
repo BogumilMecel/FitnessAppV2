32ba27b30b23e0860688195c6b288ababbdc0669
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-15 07:34:26 +0200

    Change time zone header

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/ApiConstants.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/util/ApiConstants.kt
@@ -4,7 +4,7 @@ object ApiConstants {
     object Headers {
         const val COUNTRY = "country"
         const val CURRENCY = "currency"
-        const val TIMEZONE = "timezone"
+        const val TIMEZONE = "time_zone"
         const val DEVICE_ID = "device_id"
     }
 }
