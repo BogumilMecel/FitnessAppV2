bf4decb1858d04f9e2d8cd7af3cbe6bd6173add3
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-22 11:32:15 +0200

    Up dependencies kotlin and gradle version

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/diary/GetDiaryEntriesUseCase.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/diary/GetDiaryEntriesUseCase.kt
@@ -1,7 +1,9 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.diary
 
+import android.util.Log
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.DiaryItem
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.Resource
+import com.gmail.bogumilmecel2.fitnessappv2.common.util.extensions.TAG
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.MealName
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.repository.DiaryRepository
 
@@ -15,6 +17,7 @@ class GetDiaryEntriesUseCase(
     ): Resource<Map<MealName, List<DiaryItem>>> {
         return when (val resource = diaryRepository.getDiaryEntries(date = date)) {
             is Resource.Success -> {
+                Log.e(TAG, resource.data.toString())
                 Resource.Success(
                     data = sortDiaryEntriesUseCase(
                         diaryEntries = getDiaryEntriesListFromResponseUseCase(diaryEntriesResponse = resource.data)
