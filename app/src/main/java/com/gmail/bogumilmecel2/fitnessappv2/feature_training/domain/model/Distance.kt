e06dc0e05462dbeea794429e768d1f9a22a2f0a6
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-10-28 15:12:26 +0200

    Implemented serializable on distance class

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_training/domain/model/Distance.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_training/domain/model/Distance.kt
@@ -1,6 +1,13 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_training.domain.model
 
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Distance(
+    @SerializedName("distance")
     val distance: Int,
+
+    @SerializedName("distance_unit")
     val distanceUnit: DistanceUnit
 )
