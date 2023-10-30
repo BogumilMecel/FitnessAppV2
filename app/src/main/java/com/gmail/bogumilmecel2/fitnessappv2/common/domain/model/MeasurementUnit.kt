89cdbc2426d73c343d1d8d4bb83a72321e90862c
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-10-30 08:58:00 +0100

    Added serialized names to the rest of product subclasses

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/MeasurementUnit.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/MeasurementUnit.kt
@@ -1,19 +1,23 @@
 package com.gmail.bogumilmecel2.fitnessappv2.common.domain.model
 
 import com.gmail.bogumilmecel2.fitnessappv2.R
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 enum class MeasurementUnit {
+    @SerializedName("grams")
     GRAMS,
+
+    @SerializedName("milliliters")
     MILLILITERS;
 
-    fun getStringRes() = when(this) {
+    fun getStringRes() = when (this) {
         GRAMS -> R.string.measurement_unit_gram
         MILLILITERS -> R.string.measurement_unit_milliliter
     }
 
-    fun getStringResWithValue() = when(this) {
+    fun getStringResWithValue() = when (this) {
         GRAMS -> R.string.measurement_unit_gram_with_value
         MILLILITERS -> R.string.measurement_unit_milliliter_with_value
     }
