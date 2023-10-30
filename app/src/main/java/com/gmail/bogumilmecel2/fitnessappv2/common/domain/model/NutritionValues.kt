f2762eeec4232d1f881a27b2b4fe7a4b3a6217d5
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-10-30 08:58:00 +0100

    Added serialized names to the rest of product subclasses

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/NutritionValues.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/NutritionValues.kt
@@ -1,12 +1,20 @@
 package com.gmail.bogumilmecel2.fitnessappv2.common.domain.model
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class  NutritionValues(
+data class NutritionValues(
+    @SerializedName("calories")
     val calories: Int = 0,
+
+    @SerializedName("carbohydrates")
     val carbohydrates: Double = 0.0,
+
+    @SerializedName("protein")
     val protein: Double = 0.0,
+
+    @SerializedName("fat")
     val fat: Double = 0.0
 )
 
