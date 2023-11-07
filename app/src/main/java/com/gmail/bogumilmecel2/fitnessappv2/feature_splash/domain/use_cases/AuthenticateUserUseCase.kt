f556c7d3a67c55a7a54a02965b54b0414c1b73f6
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-11-07 22:48:52 +0100

    Still working on offline mode

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_splash/domain/use_cases/AuthenticateUserUseCase.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_splash/domain/use_cases/AuthenticateUserUseCase.kt
@@ -3,63 +3,46 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_splash.domain.use_cases
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.CachedValuesProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.repository.TokenRepository
 import com.gmail.bogumilmecel2.fitnessappv2.common.presentation.util.BottomBarScreen
-import com.gmail.bogumilmecel2.fitnessappv2.common.util.Resource
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.GetUserDiaryAndSaveItLocallyUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.GetUserDiaryEntriesExperimentalUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_splash.domain.repository.LoadingRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.invoke
-import java.net.ConnectException
-import java.net.SocketTimeoutException
 
 class AuthenticateUserUseCase(
     private val cachedValuesProvider: CachedValuesProvider,
     private val loadingRepository: LoadingRepository,
     private val tokenRepository: TokenRepository,
     private val getUserDiaryAndSaveItLocallyUseCase: GetUserDiaryAndSaveItLocallyUseCase,
-    private val getUserDiaryEntriesExperimentalUseCase: GetUserDiaryEntriesExperimentalUseCase
+    private val getUserDiaryEntriesExperimentalUseCase: GetUserDiaryEntriesExperimentalUseCase,
 ) {
     suspend operator fun invoke(): Result {
         tokenRepository.getToken().data ?: return Result.NavigateToLogin
 
-        val resource = loadingRepository.authenticateUser()
+        if (!cachedValuesProvider.getOfflineMode().isOffline()) {
+            val resource = loadingRepository.authenticateUser()
 
-        val offlineModeExceptions = listOf(
-            SocketTimeoutException::class,
-            ConnectException::class
-        )
+            val user = resource.data ?: return Result.NavigateToLogin
 
-        when (resource) {
-            is Resource.Error -> {
-                val cachedUser = cachedValuesProvider.getUser()
+            cachedValuesProvider.saveUser(user = user)
 
-                return if (resource is Resource.ComplexError
-                    && (resource.exception::class in offlineModeExceptions)
-                    && cachedUser != null
-                    && cachedUser.userInformation != null
-                    && cachedUser.nutritionValues != null
-                ) {
-                    Result.NavigateToMainScreen()
-                } else {
-                    Result.NavigateToLogin
+            return if (user.nutritionValues != null && user.userInformation != null) {
+                (Dispatchers.IO) {
+                    getUserDiaryAndSaveItLocallyUseCase()
+                    getUserDiaryEntriesExperimentalUseCase()
                 }
+
+                Result.NavigateToMainScreen()
+            } else {
+                Result.NavigateToIntroduction
             }
+        } else {
+            val cachedUser = cachedValuesProvider.getUser()
 
-            is Resource.Success -> {
-                val user = resource.data ?: return Result.NavigateToLogin
-
-                cachedValuesProvider.saveUser(user = user)
-
-                return if (user.nutritionValues != null && user.userInformation != null) {
-                    (Dispatchers.IO) {
-                        getUserDiaryAndSaveItLocallyUseCase()
-                        getUserDiaryEntriesExperimentalUseCase()
-                    }
-
-                    Result.NavigateToMainScreen()
-                } else {
-                    Result.NavigateToIntroduction
-                }
+            return if (cachedUser?.userInformation != null && cachedUser.nutritionValues != null) {
+                Result.NavigateToMainScreen()
+            } else {
+                Result.NavigateToLogin
             }
         }
     }
