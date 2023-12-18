34abeefff60bf859cd78799a279116bfad8933c8
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:26:36 +0100

    reimplemented auth repository

--- app/src/main/java/com/gmail/bodziowaty6978/fitnessappv2/common/domain/model/NutritionValues.kt
+++ app/src/main/java/com/gmail/bodziowaty6978/fitnessappv2/common/domain/model/NutritionValues.kt
@@ -1,7 +1,10 @@
 package com.gmail.bodziowaty6978.fitnessappv2.common.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
+@Parcelize
 @Serializable
 data class NutritionValues(
     val id:Int = 0,
@@ -9,4 +12,4 @@ data class NutritionValues(
     val carbohydrates:Double = 100.0,
     val protein:Double = 50.0,
     val fat:Double = 10.0
-)
+):Parcelable
--- app/src/main/java/com/gmail/bodziowaty6978/fitnessappv2/feature_auth/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/gmail/bodziowaty6978/fitnessappv2/feature_auth/domain/repository/AuthRepository.kt
@@ -1,13 +1,15 @@
 package com.gmail.bodziowaty6978.fitnessappv2.feature_auth.domain.repository
 
 import com.gmail.bodziowaty6978.fitnessappv2.common.util.CustomResult
+import com.gmail.bodziowaty6978.fitnessappv2.common.util.Resource
+import com.gmail.bodziowaty6978.fitnessappv2.feature_auth.domain.model.TokenResponse
 
 interface AuthRepository {
 
     suspend fun logInUser(
         email: String,
         password: String
-    ): CustomResult
+    ): Resource<TokenResponse>
 
     suspend fun registerUser(
         email: String,
