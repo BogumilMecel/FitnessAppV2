e51b2ecda99b82a2cd3f18414f2fe27c03c6cf41
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:28:04 +0100

    Added serialized names to the rest of product subclasses

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/product/NutritionValuesIn.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/product/NutritionValuesIn.kt
@@ -1,10 +1,19 @@
 package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.product
 
+import com.google.gson.annotations.SerializedName
+
 enum class NutritionValuesIn {
-    HUNDRED_GRAMS, CONTAINER, AVERAGE;
+    @SerializedName("hundred_grams")
+    HUNDRED_GRAMS,
+
+    @SerializedName("container")
+    CONTAINER,
+
+    @SerializedName("average")
+    AVERAGE;
 
     companion object {
-        fun getValueFromIndex(index: Int) = when(index) {
+        fun getValueFromIndex(index: Int) = when (index) {
             1 -> CONTAINER
             2 -> AVERAGE
             else -> HUNDRED_GRAMS
