c00aa4b99c89a82b9f9d9c038687db4b47ea2dc6
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:09 +0100

    small work

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/CachedValuesProvider.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/CachedValuesProvider.kt
@@ -4,11 +4,11 @@ import com.gmail.bogumilmecel2.fitnessappv2.feature_summary.domain.model.LogEntr
 import com.gmail.bogumilmecel2.fitnessappv2.feature_weight.domain.model.WeightEntry
 
 interface CachedValuesProvider {
-    fun getWantedNutritionValues(): NutritionValues
-    fun saveWantedNutritionValues(nutritionValues: NutritionValues)
-    fun getWeightProgress(): String
-    fun updateWeightIProgress()
-    fun getLatestLogEntry(): LogEntry
-    fun getLatestWeightEntry(): WeightEntry
-    fun updateLatestWeightEntry()
+    suspend fun getWantedNutritionValues(): NutritionValues
+    suspend fun saveWantedNutritionValues(nutritionValues: NutritionValues)
+    suspend fun getWeightProgress(): String
+    suspend fun updateWeightIProgress()
+    suspend fun getLatestLogEntry(): LogEntry
+    suspend fun getLatestWeightEntry(): WeightEntry
+    suspend fun updateLatestWeightEntry()
 }
