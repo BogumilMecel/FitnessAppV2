32050d8031d8e8c15b15a522bd4294be1ce58073
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:54 +0100

    Work on barcode and loader in search screen

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/presentation/MainActivity.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/presentation/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.gmail.bogumilmecel2.fitnessappv2.common.presentation.navigation.NavHostGraph
 import com.gmail.bogumilmecel2.ui.theme.FitnessAppTheme
+import com.google.android.gms.common.moduleinstall.ModuleInstall
+import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +16,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val moduleInstall = ModuleInstall.getClient(this)
+        val moduleInstallRequest = ModuleInstallRequest.newBuilder()
+            .addApi(GmsBarcodeScanning.getClient(this))
+            .build()
+        moduleInstall.installModules(moduleInstallRequest)
+
         setContent {
             FitnessAppTheme {
                 NavHostGraph(navController = rememberNavController())
