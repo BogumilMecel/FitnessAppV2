81b13065c32e9754930db413520ba2e78e4d9d34
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:40 +0100

    Format product and recipe models

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/recipe/Recipe.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/recipe/Recipe.kt
@@ -11,16 +11,39 @@ import kotlinx.serialization.Serializable
 @Entity
 @Serializable
 data class Recipe(
-    @PrimaryKey val id: String = "",
-    @ColumnInfo(name = "name") val name: String = "",
-    @ColumnInfo(name = "ingredients") val ingredients: List<Ingredient> = emptyList(),
-    @ColumnInfo(name = "utc_timestamp") val utcTimestamp: Long = System.currentTimeMillis(),
-    @ColumnInfo(name = "image_url") val imageUrl: String? = null,
-    @ColumnInfo(name = "nutrition_values") val nutritionValues: NutritionValues = NutritionValues(),
-    @ColumnInfo(name = "time_required") val timeRequired: TimeRequired = TimeRequired.LOW,
-    @ColumnInfo(name = "difficulty") val difficulty: Difficulty = Difficulty.LOW,
-    @ColumnInfo(name = "servings") val servings: Int = 0,
-    @ColumnInfo(name = "is_public") val isPublic: Boolean = false,
-    @ColumnInfo(name = "username") val username: String = "",
-    @ColumnInfo(name = "user_id") val userId: String = "",
+    @PrimaryKey
+    val id: String = "",
+
+    @ColumnInfo(name = "name")
+    val name: String = "",
+
+    @ColumnInfo(name = "ingredients")
+    val ingredients: List<Ingredient> = emptyList(),
+
+    @ColumnInfo(name = "utc_timestamp")
+    val utcTimestamp: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "image_url")
+    val imageUrl: String? = null,
+
+    @ColumnInfo(name = "nutrition_values")
+    val nutritionValues: NutritionValues = NutritionValues(),
+
+    @ColumnInfo(name = "time_required")
+    val timeRequired: TimeRequired = TimeRequired.LOW,
+
+    @ColumnInfo(name = "difficulty")
+    val difficulty: Difficulty = Difficulty.LOW,
+
+    @ColumnInfo(name = "servings")
+    val servings: Int = 0,
+
+    @ColumnInfo(name = "is_public")
+    val isPublic: Boolean = false,
+
+    @ColumnInfo(name = "username")
+    val username: String = "",
+
+    @ColumnInfo(name = "user_id")
+    val userId: String = "",
 )
