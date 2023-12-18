1bde3bcc1dbc9801a44c6cb35cc74ad22cb75924
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:37 +0100

    Turn diary entries to room entities

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/diary_entry/ProductDiaryEntry.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/diary_entry/ProductDiaryEntry.kt
@@ -2,6 +2,9 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.diary_en
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.DiaryItem
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.MeasurementUnit
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
@@ -9,17 +12,37 @@ import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.DiaryEntr
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.MealName
 import kotlinx.serialization.Serializable
 
+@Entity
 @Serializable
 data class ProductDiaryEntry(
+    @PrimaryKey
     override val id: String = "",
+
+    @ColumnInfo("nutrition_values")
     override val nutritionValues: NutritionValues = NutritionValues(),
+
+    @ColumnInfo("utc_timestamp")
     override val utcTimestamp: Long = 0,
+
+    @ColumnInfo("user_id")
     override val userId: String = "",
+
+    @ColumnInfo("date")
     override val date: String = "",
+
+    @ColumnInfo("meal_name")
     override val mealName: MealName = MealName.BREAKFAST,
+
+    @ColumnInfo("product_measurement_unit")
     val productMeasurementUnit: MeasurementUnit = MeasurementUnit.GRAMS,
+
+    @ColumnInfo("product_name")
     val productName: String = "",
+
+    @ColumnInfo("product_id")
     val productId: String = "",
+
+    @ColumnInfo("weight")
     val weight: Int = 0,
 ) : DiaryItem {
     @Composable
--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/recipe/RecipeDiaryEntry.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/model/recipe/RecipeDiaryEntry.kt
@@ -2,6 +2,9 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.recipe
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.pluralStringResource
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.gmail.bogumilmecel2.fitnessappv2.R
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.DiaryItem
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
@@ -9,16 +12,34 @@ import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.DiaryEntr
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.MealName
 import kotlinx.serialization.Serializable
 
+@Entity
 @Serializable
 data class RecipeDiaryEntry(
+    @PrimaryKey
     override val id: String = "",
+
+    @ColumnInfo("nutrition_values")
     override val nutritionValues: NutritionValues = NutritionValues(),
+
+    @ColumnInfo("utc_timestamp")
     override val utcTimestamp: Long = 0,
+
+    @ColumnInfo("user_id")
     override val userId: String = "",
-    override val mealName: MealName = MealName.BREAKFAST,
+
+    @ColumnInfo("date")
     override val date: String = "",
+
+    @ColumnInfo("meal_name")
+    override val mealName: MealName = MealName.BREAKFAST,
+
+    @ColumnInfo("recipe_name")
     val recipeName: String = "",
+
+    @ColumnInfo("recipe_id")
     val recipeId: String = "",
+
+    @ColumnInfo("servings")
     val servings: Int = 0,
 ) : DiaryItem {
     @Composable
