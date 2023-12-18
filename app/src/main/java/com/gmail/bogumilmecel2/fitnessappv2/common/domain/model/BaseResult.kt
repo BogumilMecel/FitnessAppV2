32a82825cff0eba7fa7e05a6fede08f361fc2578
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:28:08 +0100

    Fixes after migrating weight dialog

---
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/domain/model/BaseResult.kt
@@ -0,0 +1,9 @@
+package com.gmail.bogumilmecel2.fitnessappv2.common.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+enum class BaseResult {
+    Success,
+    Canceled
+}
