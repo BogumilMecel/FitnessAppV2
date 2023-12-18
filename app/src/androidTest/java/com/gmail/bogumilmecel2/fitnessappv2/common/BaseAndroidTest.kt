bda5b1d33e433f9b0a2e51d9bdcb3373b7949509
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:59 +0100

    Added some utils to android tests

--- app/src/androidTest/java/com/gmail/bogumilmecel2/fitnessappv2/common/BaseAndroidTest.kt
+++ app/src/androidTest/java/com/gmail/bogumilmecel2/fitnessappv2/common/BaseAndroidTest.kt
@@ -4,12 +4,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.gmail.bogumilmecel2.fitnessappv2.common.presentation.MainActivity
 import com.gmail.bogumilmecel2.fitnessappv2.common.presentation.navigation.NavHostGraph
+import com.gmail.bogumilmecel2.fitnessappv2.common.util.TestTags
 import com.gmail.bogumilmecel2.fitnessappv2.destinations.Destination
 import com.gmail.bogumilmecel2.ui.theme.FitnessAppTheme
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -32,18 +38,53 @@ open class BaseAndroidTest {
 
     private lateinit var navController: TestNavHostController
 
-    fun setContent(destination: Destination) {
-        composeRule.activity.setContent {
-            navController = TestNavHostController(LocalContext.current)
-            navController.navigatorProvider.addNavigator(ComposeNavigator())
-            FitnessAppTheme {
-                NavHostGraph(
-                    startDestination = destination,
-                    navController = navController
-                )
+    fun setContent(
+        destination: Destination,
+        screenTestTag: String? = null
+    ) {
+        composeRule.run {
+            activity.setContent {
+                navController = TestNavHostController(LocalContext.current)
+                navController.navigatorProvider.addNavigator(ComposeNavigator())
+                FitnessAppTheme {
+                    NavHostGraph(
+                        startDestination = destination,
+                        navController = navController
+                    )
+                }
+            }
+
+            screenTestTag?.let {
+                onNodeWithTag(screenTestTag).assertIsDisplayed()
             }
         }
     }
 
-    fun SemanticsNodeInteraction.assertHasText(text: String) = assert(hasText(text = text))
+    fun performTextInputAndAssertIsDisplayed(
+        testTag: String,
+        text: String,
+        expectedTest: String? = null
+    ) {
+        composeRule.onNodeWithTag(testTag).run {
+            assertIsDisplayed()
+            performTextInput(text)
+            assertHasText(expectedTest ?: text)
+        }
+    }
+
+    fun performButtonClickAndAssertSnackbarDisplayed(
+        buttonTestTag: String,
+        snackbarText: String? = null
+    ) {
+        composeRule.run {
+            onNodeWithTag(buttonTestTag).performClick()
+            onNodeWithTag(TestTags.General.SNACKBAR).assertIsDisplayed()
+
+            snackbarText?.let {
+                onNodeWithText(it).assertIsDisplayed()
+            }
+        }
+    }
+
+    private fun SemanticsNodeInteraction.assertHasText(text: String) = assert(hasText(text = text))
 }
