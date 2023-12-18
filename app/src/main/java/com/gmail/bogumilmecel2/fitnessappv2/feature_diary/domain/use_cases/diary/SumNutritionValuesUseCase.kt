edbac9f40977152cac2b6cd69924088aa5966892
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-12-18 17:27:36 +0100

    Fix diary

--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/diary/SumNutritionValuesUseCase.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/domain/use_cases/diary/SumNutritionValuesUseCase.kt
@@ -2,7 +2,7 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.diar
 
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
 
-class CalculateNutritionValuesFromNutritionValuesUseCase {
+class SumNutritionValuesUseCase {
     operator fun invoke(nutritionValues: List<NutritionValues>) = NutritionValues(
         calories = nutritionValues.sumOf { it.calories },
         carbohydrates = nutritionValues.sumOf { it.carbohydrates },
--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/presentation/diary/DiaryState.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/feature_diary/presentation/diary/DiaryState.kt
@@ -2,19 +2,17 @@ package com.gmail.bogumilmecel2.fitnessappv2.feature_diary.presentation.diary
 
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.DiaryItem
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
+import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.Meal
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.MealName
 
 data class DiaryState(
-    val diaryEntries: Map<MealName, List<DiaryItem>> = mutableMapOf(),
+    val diaryEntries: Map<MealName, Meal> = emptyMap(),
     val wantedTotalNutritionValues: NutritionValues = NutritionValues(),
     val currentTotalNutritionValues: NutritionValues = NutritionValues(),
-    val mealNutritionValues: Map<MealName, NutritionValues> = mutableMapOf(),
     val longClickedDiaryItemParams: LongClickedDiaryItemParams? = null,
     val currentlySelectedMealName: MealName? = null,
-    val displayedDate: String = ""
-) {
-    fun getDiaryEntries(mealName: MealName) = diaryEntries[mealName] ?: emptyList()
-}
+    val displayedDate: String = "",
+)
 
 data class LongClickedDiaryItemParams(
     val longClickedDiaryItem: DiaryItem,
