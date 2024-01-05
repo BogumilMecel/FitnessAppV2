d358b4a9e8dd7e74537a97737fba95d9db70bd89
Bogumił Męcel <Bodziowaty6978@gmail.com>
2024-01-05 07:14:14 +0100

    Fix TestAppModule

--- app/src/androidTest/java/com/gmail/bogumilmecel2/fitnessappv2/common/di/TestAppModule.kt
+++ app/src/androidTest/java/com/gmail/bogumilmecel2/fitnessappv2/common/di/TestAppModule.kt
@@ -6,16 +6,13 @@ import com.gmail.bogumilmecel2.fitnessappv2.common.MockCachedValuesProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.connectivity.ConnectivityObserver
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.ConnectionState
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.Currency
-import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.DiaryItem
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.model.NutritionValues
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.CachedValuesProvider
-import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.DateHolder
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.provider.ResourceProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.repository.TokenRepository
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.use_case.CalculateNutritionValuesPercentages
 import com.gmail.bogumilmecel2.fitnessappv2.common.domain.use_case.GetUserCurrencyUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.BarcodeScanner
-import com.gmail.bogumilmecel2.fitnessappv2.common.util.RealDateHolder
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.RealResourceProvider
 import com.gmail.bogumilmecel2.fitnessappv2.common.util.Resource
 import com.gmail.bogumilmecel2.fitnessappv2.feature_auth.data.repository.MockAuthRepository
@@ -27,16 +24,12 @@ import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.DiaryEntr
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.Product
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.ProductPrice
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.RecipePriceResponse
-import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.UserDiaryItemsResponse
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.diary_entry.ProductDiaryEntry
-import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.diary_entry.ProductDiaryEntryPostRequest
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.product.NewPriceRequest
-import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.product.NewProductRequest
-import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.recipe.NewRecipeRequest
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.recipe.Recipe
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.recipe.RecipeDiaryEntry
-import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.model.recipe.RecipeDiaryEntryRequest
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.repository.DiaryRepository
+import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.repository.OfflineDiaryRepository
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CalculateSkipUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CreateSearchItemParamsFromIngredientUseCase
 import com.gmail.bogumilmecel2.fitnessappv2.feature_diary.domain.use_cases.CreateSearchItemParamsFromProductUseCase
@@ -66,6 +59,8 @@ import dagger.hilt.testing.TestInstallIn
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
 import javax.inject.Singleton
 
 @Module
@@ -86,10 +81,6 @@ object TestAppModule {
     @Provides
     @Singleton
     fun provideResourceProvider(app: Application): ResourceProvider = RealResourceProvider(app)
-
-    @Singleton
-    @Provides
-    fun provideDateProvider(): DateHolder = RealDateHolder()
 
     @Singleton
     @Provides
@@ -122,14 +113,140 @@ object TestAppModule {
     @Provides
     fun provideConnectivityObserver(): ConnectivityObserver = object : ConnectivityObserver {
         override fun observe(): Flow<ConnectionState> = callbackFlow {
-            awaitClose {  }
+            awaitClose { }
+        }
+
+        override suspend fun isOnline(): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOfflineDiaryRepository(): OfflineDiaryRepository = object : OfflineDiaryRepository {
+        override suspend fun getProducts(
+            userId: String?,
+            searchText: String?,
+            limit: Long,
+            skip: Long
+        ): Resource<List<Product>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getProduct(productId: String): Resource<Product?> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertProducts(products: List<Product>): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertProduct(product: Product): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getProductDiaryEntries(
+            searchText: String?,
+            limit: Long,
+            skip: Long
+        ): Resource<List<ProductDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getProductDiaryEntries(limit: Long): Resource<List<ProductDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getProductDiaryEntries(date: LocalDate): Resource<List<ProductDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertProductDiaryEntries(productDiaryEntries: List<ProductDiaryEntry>): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertProductDiaryEntry(productDiaryEntry: ProductDiaryEntry): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun deleteProductDiaryEntries(date: LocalDate): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun deleteProductDiaryEntry(productDiaryEntryId: String): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipes(
+            userId: String?,
+            searchText: String?,
+            limit: Long,
+            skip: Long
+        ): Resource<List<Recipe>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipe(recipeId: String): Resource<Recipe?> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertRecipes(recipes: List<Recipe>): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertRecipe(recipe: Recipe): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipeDiaryEntries(
+            searchText: String?,
+            limit: Long,
+            skip: Long
+        ): Resource<List<RecipeDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipeDiaryEntries(limit: Long): Resource<List<RecipeDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipeDiaryEntries(date: LocalDate): Resource<List<RecipeDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertRecipeDiaryEntries(recipeDiaryEntries: List<RecipeDiaryEntry>): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun insertRecipeDiaryEntry(recipeDiaryEntry: RecipeDiaryEntry): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun deleteRecipeDiaryEntries(date: LocalDate): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun deleteRecipeDiaryEntry(recipeDiaryEntryId: String): Resource<Unit> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getDiaryEntriesNutritionValues(date: LocalDate): Resource<List<NutritionValues>> {
+            TODO("Not yet implemented")
         }
     }
 
     @Singleton
     @Provides
     fun provideDiaryRepository(): DiaryRepository = object : DiaryRepository {
-        override suspend fun getDiaryEntries(date: String): Resource<DiaryEntriesResponse> {
+        override suspend fun getDiaryEntries(date: LocalDate): Resource<DiaryEntriesResponse> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getProductDiaryEntries(latestDateTime: LocalDateTime?): Resource<List<ProductDiaryEntry>> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun getRecipeDiaryEntries(latestDateTime: LocalDateTime?): Resource<List<RecipeDiaryEntry>> {
             TODO("Not yet implemented")
         }
 
@@ -137,40 +254,6 @@ object TestAppModule {
             searchText: String,
             page: Int
         ): Resource<List<Product>> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getLocalUserRecipes(
-            userId: String,
-            searchText: String?,
-            limit: Int,
-            skip: Int
-        ): Resource<List<Recipe>> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getLocalUserProducts(
-            userId: String,
-            searchText: String?,
-            limit: Int,
-            skip: Int
-        ): Resource<List<Product>> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getDiaryEntriesComplete(): Resource<DiaryEntriesResponse> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getOfflineDiaryEntries(date: String): Resource<DiaryEntriesResponse> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun insertOfflineDiaryEntries(diaryEntriesResponse: DiaryEntriesResponse): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getDiaryEntriesCount(): Resource<Int> {
             TODO("Not yet implemented")
         }
 
@@ -182,11 +265,11 @@ object TestAppModule {
             TODO("Not yet implemented")
         }
 
-        override suspend fun insertProductDiaryEntry(productDiaryEntryPostRequest: ProductDiaryEntryPostRequest): Resource<ProductDiaryEntry> {
+        override suspend fun insertProductDiaryEntry(productDiaryEntry: ProductDiaryEntry): Resource<ProductDiaryEntry> {
             TODO("Not yet implemented")
         }
 
-        override suspend fun insertRecipeDiaryEntry(recipeDiaryEntryRequest: RecipeDiaryEntryRequest): Resource<RecipeDiaryEntry> {
+        override suspend fun insertRecipeDiaryEntry(recipeDiaryEntry: RecipeDiaryEntry): Resource<RecipeDiaryEntry> {
             TODO("Not yet implemented")
         }
 
@@ -198,18 +281,6 @@ object TestAppModule {
             TODO("Not yet implemented")
         }
 
-        override suspend fun deleteOfflineDiaryEntry(diaryItem: DiaryItem): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun deleteOfflineDiaryEntries(
-            date: String,
-            productDiaryEntriesIds: List<String>,
-            recipeDiaryEntriesIds: List<String>
-        ): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
         override suspend fun editProductDiaryEntry(productDiaryEntry: ProductDiaryEntry): Resource<ProductDiaryEntry> {
             TODO("Not yet implemented")
         }
@@ -218,15 +289,11 @@ object TestAppModule {
             TODO("Not yet implemented")
         }
 
-        override suspend fun saveNewProduct(newProductRequest: NewProductRequest): Resource<Product> {
+        override suspend fun saveNewProduct(product: Product): Resource<Product> {
             TODO("Not yet implemented")
         }
 
         override suspend fun getProduct(productId: String): Resource<Product?> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getDiaryEntriesNutritionValues(date: String): Resource<List<NutritionValues>> {
             TODO("Not yet implemented")
         }
 
@@ -252,52 +319,15 @@ object TestAppModule {
             TODO("Not yet implemented")
         }
 
-        override suspend fun addNewRecipe(newRecipeRequest: NewRecipeRequest): Resource<Recipe> {
+        override suspend fun addNewRecipe(recipe: Recipe): Resource<Recipe> {
             TODO("Not yet implemented")
         }
 
-        override suspend fun getOfflineDiaryEntries(
-            limit: Int,
-            offset: Int,
-            searchText: String
-        ): Resource<List<ProductDiaryEntry>> {
+        override suspend fun getUserProducts(latestDateTime: LocalDateTime?): Resource<List<Product>> {
             TODO("Not yet implemented")
         }
 
-        override suspend fun getUserDiaryItems(): Resource<UserDiaryItemsResponse> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun insertUserProductsLocally(userProducts: List<Product>): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun insertUserRecipesLocally(userRecipes: List<Recipe>): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-
-        override suspend fun clearLocalData(userId: String): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun insertOfflineDiaryEntry(diaryItem: DiaryItem): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun cacheProduct(product: Product): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun cacheRecipe(recipe: Recipe): Resource<Unit> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getOfflineProduct(productId: String): Resource<Product?> {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun getOfflineRecipe(recipeId: String): Resource<Recipe?> {
+        override suspend fun getUserRecipes(latestDateTime: LocalDateTime?): Resource<List<Recipe>> {
             TODO("Not yet implemented")
         }
     }
@@ -363,13 +393,25 @@ object TestAppModule {
 
     @Singleton
     @Provides
-    fun provideGetProductUseCase(diaryRepository: DiaryRepository): GetProductUseCase =
-        GetProductUseCase(diaryRepository)
+    fun provideGetProductUseCase(
+        diaryRepository: DiaryRepository,
+        offlineDiaryRepository: OfflineDiaryRepository
+    ): GetProductUseCase =
+        GetProductUseCase(
+            diaryRepository = diaryRepository,
+            offlineDiaryRepository = offlineDiaryRepository
+        )
 
     @Singleton
     @Provides
-    fun provideGetRecipeUseCase(diaryRepository: DiaryRepository): GetRecipeUseCase =
-        GetRecipeUseCase(diaryRepository)
+    fun provideGetRecipeUseCase(
+        diaryRepository: DiaryRepository,
+        offlineDiaryRepository: OfflineDiaryRepository
+    ): GetRecipeUseCase =
+        GetRecipeUseCase(
+            diaryRepository = diaryRepository,
+            offlineDiaryRepository = offlineDiaryRepository
+        )
 
     @Singleton
     @Provides
--- app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/di/AppModule.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/fitnessappv2/common/di/AppModule.kt
@@ -213,7 +213,7 @@ object AppModule {
     ): Retrofit {
         val retrofitBuilder = Retrofit.Builder()
             .client(httpClient)
-            .baseUrl("http://192.168.1.157:8080/products/")
+            .baseUrl("http://192.168.0.216:8080/")
             .addConverterFactory(GsonConverterFactory.create())
         return retrofitBuilder.build()
     }
